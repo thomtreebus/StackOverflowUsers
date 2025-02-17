1df21fe801ed4547aa4e410ce029f9862658c1b5
Thom Treebus <thomtreebus@gmail.com>
2025-02-17 17:50:57 +0000

    Initial Commit

---
+++ StackOverflowUsers/StackOverflowUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackOverflowUsersApp.swift
+//  StackOverflowUsers
+//
+//  Created by Thom Treebus on 17/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct StackOverflowUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
