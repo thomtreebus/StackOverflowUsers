53cd13c9ade256574694edae69cff0c6848daf8f
Thom Treebus <thomtreebus@gmail.com>
2025-02-22 14:30:08 +0000

    add image and text to Error view

--- StackOverflowUsers/Views/ErrorView.swift
+++ StackOverflowUsers/Views/ErrorView.swift
@@ -3,10 +3,35 @@ import SwiftUI
 struct ErrorView: View {
     let message: String
     let retryAction: () -> Void
-    
+
     var body: some View {
         VStack(spacing: 16) {
-            Text("error, could not load users")
+            Image(systemName: "exclamationmark.circle")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 80)
+                .foregroundColor(.red)
+
+            Text("Error loading users")
+                .font(.title2)
+                .fontWeight(.bold)
+
+            Text(message)
+                .font(.body)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 32)
+                .foregroundColor(.gray)
+
+            Button(action: retryAction) {
+                Text("Try Again")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 24)
+                    .padding(.vertical, 12)
+                    .background(.blue)
+                    .cornerRadius(8)
+            }
+            .padding(.top, 8)
         }
     }
 }
